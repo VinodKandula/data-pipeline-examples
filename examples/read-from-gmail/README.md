1cb945acec1a3907d93141c1730b5933b234f3f9
jpllosa <jopeellosa@gmail.com>
2017-08-11 11:58:53 +0100

    added read from gmail example

---
+++ examples/read-from-gmail/README.md
@@ -0,0 +1,108 @@
+# Read From Gmail
+
+How To Export Emails From Gmail To Excel With Data Pipeline.
+
+## How to run the example
+1. Clone the repo.
+2. Download the Data Pipeline jar at [North Concepts Inc.][1]
+3. Place the **NorthConcepts-DataPipeline-x.x.x.jar** under the */libs* folder.
+4. Place the license file under the */src/test/resources* folder.
+5. Execute **mvn clean** at the parent root directory.
+6. Change to the example root directory (e.g., *examples/read-from-gmail*).
+7. To run the individual methods, **mvn -Dtest=ReadFromGmail#<method name> test** (e.g., **mvn -Dtest=ReadFromGmail#readLatest10Emails test**).
+8. To run all the methods, **mvn -Dtest=ReadFromGmail#<method name> test**.
+9. Check out the [How To Export Emails From Gmail To Excel With Data Pipeline][2] blog for a more detailed explanation.
+
+[1]: https://northconcepts.com/pricing/ "Data Pipeline Download"
+[2]: https://northconcepts.com/blog/2017/08/10/export-emails-to-excel/ "How To Export Emails From Gmail To Excel With Data Pipeline"
+
+## Output
+Looks something like this:
+```sh
+-----------------------------------------------
+0 - Record (MODIFIED) {
+    0:[from]:STRING=[Google <no-reply@accounts.google.com>]:String
+    1:[fromName]:STRING=[Google]:String
+    2:[fromAddress]:STRING=[no-reply@accounts.google.com]:String
+    3:[replyTo]:STRING=[Google <no-reply@accounts.google.com>]:String
+    4:[replyToName]:STRING=[Google]:String
+    5:[replyToAddress]:STRING=[no-reply@accounts.google.com]:String
+    6:[to]:STRING=[user@example.com]:String
+    7:[toName]:UNDEFINED=[null]
+    8:[toAddress]:STRING=[user@example.com]:String
+    9:[sentDate]:DATETIME=[Fri Aug 11 11:12:59 BST 2017]:Date
+    10:[subject]:STRING=[How To Export Emails From Gmail To Excel With Data Pipeline]:String
+    11:[size]:INT=[12155]:Integer
+    12:[folderName]:STRING=[INBOX]:String
+    13:[folderFullName]:STRING=[INBOX]:String
+    14:[flagAnswered]:BOOLEAN=[false]:Boolean
+    15:[flagDeleted]:BOOLEAN=[false]:Boolean
+    16:[flagDraft]:BOOLEAN=[false]:Boolean
+    17:[flagFlagged]:BOOLEAN=[false]:Boolean
+    18:[flagRecent]:BOOLEAN=[false]:Boolean
+    19:[flagSeen]:BOOLEAN=[false]:Boolean
+    20:[flagUser]:BOOLEAN=[false]:Boolean
+    21:[sender]:STRING=[Google <no-reply@accounts.google.com>]:String
+    22:[senderName]:STRING=[Google]:String
+    23:[senderAddress]:STRING=[no-reply@accounts.google.com]:String
+    24:[receivedDate]:DATETIME=[Fri Aug 11 11:13:05 BST 2017]:Date
+    25:[contentType]:STRING=[multipart/ALTERNATIVE; boundary=001a113f229a1853530556778f70]:String
+    26:[contentID]:UNDEFINED=[null]
+    27:[contentLanguage]:UNDEFINED=[null]
+    28:[contentMD5]:UNDEFINED=[null]
+    29:[disposition]:UNDEFINED=[null]
+    30:[encoding]:UNDEFINED=[null]
+    31:[fileName]:UNDEFINED=[null]
+    32:[description]:UNDEFINED=[null]
+    33:[messageID]:STRING=[<wlVKpiQv5W2yZafdfyOuAQ@notifications.google.com>]:String
+    34:[messageNumber]:INT=[60]:Integer
+    35:[headerDelivered-To]:STRING=[user@example.com]:String
+    36:[headerReceived]:ARRAY of STRING=[[by 10.140.96.196 with SMTP id k62csp713462qge; Fri, 11 Aug 2017
+ 03:13:05 -0700 (PDT), from mail-yw0-x246.google.com (mail-yw0-x246.google.com.
+ [2607:f8b0:4002:c05::246]) by mx.google.com with ESMTPS id
+ n187si29...294, by mail-yw0-x246.google.com with SMTP id k20so42883023ywe.7 for
+ <user@example.com>; Fri, 11 Aug 2017 03:13:05 -0700 (PDT...129]]:ArrayValue
+    37:[headerX-Received]:ARRAY of STRING=[[by 10.129.159.4 with SMTP id w4mr12602492ywg.304.1502446385531;
+ Fri, 11 Aug 2017 03:13:05 -0700 (PDT), by 10.129.85.141 with SMTP id j135mr9276503ywb.209.1502446385385;
+ Fri, 11 Aug 2017 03:13:05 -0700 (PDT)]]:ArrayValue
+    38:[headerARC-Seal]:STRING=[i=1; a=rsa-sha256; t=1502446385; cv=none; d=google.com;
+ s=arc-20160816;
+ b=x3GaXIeY/9gxaIabSdc5YR4nDVhNlWAQQEEFceLN1VorQCVyDF...435]:String
+    39:[headerARC-Message-Signature]:STRING=[i=1; a=rsa-sha256; c=relaxed/relaxed; d=google.com;
+ s=arc-20160816; h=to:from:subject:message-id:feedback-id:date:mime-version...587]:String
+    40:[headerARC-Authentication-Results]:STRING=[i=1; mx.google.com; dkim=pass
+ header.i=@accounts.google.com header.s=20161025 header.b=Kcj0T0Ph; spf=pass
+ (google.com: domai...454]:String
+    41:[headerReturn-Path]:STRING=[<3MYONWQgTDEApq-tgrn0ceeqwpvu.iqqing.eqovctqnwnwmwtqiockn.eqo@gaia.bounces.google.com>]:String
+    42:[headerReceived-SPF]:STRING=[pass (google.com: domain of
+ 3myonwqgtdeapq-tgrn0ceeqwpvu.iqqing.eqovctqnwnwmwtqiockn.eqo@gaia.bounces.google.com
+ designates ...209]:String
+    43:[headerAuthentication-Results]:STRING=[mx.google.com; dkim=pass header.i=@accounts.google.com
+ header.s=20161025 header.b=Kcj0T0Ph; spf=pass (google.com: domain of
+ ...447]:String
+    44:[headerDKIM-Signature]:STRING=[v=1; a=rsa-sha256; c=relaxed/relaxed; d=accounts.google.com;
+ s=20161025; h=mime-version:date:feedback-id:message-id:subject:fr...547]:String
+    45:[headerX-Google-DKIM-Signature]:STRING=[v=1; a=rsa-sha256; c=relaxed/relaxed; d=1e100.net;
+ s=20161025;
+ h=x-gm-message-state:mime-version:date:feedback-id:message-id...559]:String
+    46:[headerX-Gm-Message-State]:STRING=[AHYfb5hGM7xMr/IjUvCapGbvvdwK55mGPyGWYLLoOPandiLm0I93b24o
+ t06zDlCETkH50YI5YMxMH3X/BO7ww71G]:String
+    47:[headerMIME-Version]:STRING=[1.0]:String
+    48:[headerDate]:STRING=[Fri, 11 Aug 2017 10:12:59 +0000 (UTC)]:String
+    49:[headerX-Notifications]:STRING=[XEAAAAAeUmpb6-bdoZDDy1wlk7cE]:String
+    50:[headerX-Account-Notification-Type]:STRING=[28]:String
+    51:[headerFeedback-ID]:STRING=[28:account-notifier]:String
+    52:[headerMessage-ID]:STRING=[<wlVKpiQv5W2yZafdfyOuAQ@notifications.google.com>]:String
+    53:[headerSubject]:STRING=[How To Export Emails From Gmail To Excel With Data Pipeline]:String
+    54:[headerFrom]:STRING=[Google <no-reply@accounts.google.com>]:String
+    55:[headerTo]:STRING=[user@example.com]:String
+    56:[headerContent-Type]:STRING=[multipart/alternative; boundary="001a113f229a1853530556778f70"]:String
+    57:[multipartContentType]:STRING=[multipart/ALTERNATIVE; boundary=001a113f229a1853530556778f70]:String
+    58:[contentFileName]:ARRAY of UNDEFINED=[[null, null]]:ArrayValue
+    59:[contentContentType]:ARRAY of STRING=[[TEXT/PLAIN; charset=UTF-8; delsp=yes; format=flowed, TEXT/HTML; charset=UTF-8]]:ArrayValue
+    60:[contentDisposition]:ARRAY of UNDEFINED=[[null, null]]:ArrayValue
+    61:[contentSize]:ARRAY of INT=[[1604, 5030]]:ArrayValue
+    62:[content]:ARRAY of STRING=[[Have you ever wanted to pull emails into Excel for analysis?  Maybe you need to find the top companies contacting you for your sales team.  Maybe you need to perform text or sentiment analysis on the contents of your messages.  Or maybe you’re creating visualizations to better understand who’s email you.  This quick guide will show you how to use Data Pipeline to search and read emails from Gmail or G Suite (formerly Google Apps), process them any way you like, and store them in Excel....4654]]:ArrayValue
+}
+
+```
---
+++ examples/read-from-gmail/src/test/java/com/northconcepts/examples/ReadFromGmail.java
@@ -0,0 +1,168 @@
+/*
+ * Copyright (c) 2006-2017 North Concepts Inc.  All rights reserved.
+ * Proprietary and Confidential.  Use is subject to license terms.
+ *
+ * http://northconcepts.com/data-pipeline/licensing/
+ *
+ */
+package com.northconcepts.examples;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.mail.search.AndTerm;
+import javax.mail.search.ReceivedDateTerm;
+import javax.mail.search.SubjectTerm;
+
+import org.junit.Test;
+
+import com.northconcepts.datapipeline.core.DataReader;
+import com.northconcepts.datapipeline.core.DataWriter;
+import com.northconcepts.datapipeline.core.LimitReader;
+import com.northconcepts.datapipeline.core.StreamWriter;
+import com.northconcepts.datapipeline.email.EmailReadDirection;
+import com.northconcepts.datapipeline.email.EmailReader;
+import com.northconcepts.datapipeline.email.GmailFolders;
+import com.northconcepts.datapipeline.email.MailStore;
+import com.northconcepts.datapipeline.excel.ExcelDocument;
+import com.northconcepts.datapipeline.excel.ExcelWriter;
+import com.northconcepts.datapipeline.file.FileReader;
+import com.northconcepts.datapipeline.file.FileWriter;
+import com.northconcepts.datapipeline.job.Job;
+import com.northconcepts.datapipeline.transform.BasicFieldTransformer;
+import com.northconcepts.datapipeline.transform.SelectFields;
+import com.northconcepts.datapipeline.transform.TransformingReader;
+
+public class ReadFromGmail {
+
+    private static final String HOST = "imap.gmail.com";
+    private static final String USER = "user@example.com";
+    private static final String PASSWD = "password";
+	
+    public static final SimpleDateFormat DATE_TIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    @Test
+    public void readLatest10Emails() throws Throwable {
+        DataReader reader = new EmailReader(MailStore.IMAP_OVER_SSL, HOST, USER, PASSWD)
+                .setDirection(EmailReadDirection.BACKWARD);
+        reader = new LimitReader(reader, 10);
+        DataWriter writer = StreamWriter.newSystemOutWriter();
+        Job.run(reader, writer);
+    }
+    
+    @Test
+    public void readLatest10SentEmails() throws Throwable {
+        DataReader reader = new EmailReader(MailStore.IMAP_OVER_SSL, HOST, USER, PASSWD)
+                .setDirection(EmailReadDirection.BACKWARD)
+                .setFolderName(GmailFolders.SENT_MAIL);
+        reader = new LimitReader(reader, 10);
+        DataWriter writer = StreamWriter.newSystemOutWriter();
+        Job.run(reader, writer);
+    }
+    
+    @Test
+    public void searchEmailsBySubject() throws Throwable {
+        DataReader reader = new EmailReader(MailStore.IMAP_OVER_SSL, HOST, USER, PASSWD)
+                .setSearchTerm(new SubjectTerm("Trial License"))
+                .setFolderName(GmailFolders.ALL_MAIL);
+        reader = new LimitReader(reader, 10);
+        DataWriter writer = StreamWriter.newSystemOutWriter();
+        Job.run(reader, writer);
+    }
+    
+    @Test
+    public void searchEmailsByDateAndSubject() throws Throwable {
+        DataReader reader = new EmailReader(MailStore.IMAP_OVER_SSL, HOST, USER, PASSWD)
+                .setSearchTerm(new AndTerm(
+                        new ReceivedDateTerm(ReceivedDateTerm.GE, DATE_TIME_FORMAT.parse("2017-07-01 00:00:00")),
+                        new SubjectTerm("Trial License")))
+                .setFolderName(GmailFolders.ALL_MAIL)
+                .setDirection(EmailReadDirection.BACKWARD)
+                ;
+        reader = new LimitReader(reader, 25);
+        DataWriter writer = StreamWriter.newSystemOutWriter();
+        Job.run(reader, writer);
+    }
+    
+    @Test
+    public void debugEmailReader() throws Throwable {
+        DataReader reader = new EmailReader(MailStore.IMAP_OVER_SSL, HOST, USER, PASSWD)
+                .setSearchTerm(new SubjectTerm("Trial License"))
+                .setFolderName(GmailFolders.ALL_MAIL)
+                .setDebug(true);
+        reader = new LimitReader(reader, 10);
+        DataWriter writer = StreamWriter.newSystemOutWriter();
+        Job.run(reader, writer);
+    }
+
+    @Test
+    public void exportEmailsToExcel() throws Throwable {
+        DataReader reader = new EmailReader(MailStore.IMAP_OVER_SSL, HOST, USER, PASSWD)
+                .setSearchTerm(new AndTerm(
+                        new ReceivedDateTerm(ReceivedDateTerm.GE, DATE_TIME_FORMAT.parse("2017-06-01 00:00:00")),
+                        new SubjectTerm("Trial License")))
+                .setFolderName(GmailFolders.ALL_MAIL)
+                .setDirection(EmailReadDirection.BACKWARD);
+        
+        reader = new LimitReader(reader, 100);
+        
+        reader = new TransformingReader(reader)
+                .add(new SelectFields("from", "to", "sentDate", "subject", "content"))
+                .add(new BasicFieldTransformer("content")
+                        .flattenToString("")
+                        .left(32767));
+        
+        
+        ExcelDocument document = new ExcelDocument();
+        DataWriter writer = new ExcelWriter(document).setSheetName("License Trials");
+
+        Job.run(reader, writer);
+
+        String date = new SimpleDateFormat("yyyy-MM-dd-HHmmss").format(new Date());
+        document.save(new File("scratch/data/output/trials-" + date + ".xlsx"));
+    }
+
+    @Test
+    public void dumpEmailsToFile() throws Throwable {
+        System.setProperty("mail.mime.base64.ignoreerrors", "true");
+        
+        DataReader reader = new EmailReader(MailStore.IMAP_OVER_SSL, HOST, USER, PASSWD)
+                .setSearchTerm(new AndTerm(
+                        new ReceivedDateTerm(ReceivedDateTerm.GE, DATE_TIME_FORMAT.parse("2015-01-01 00:00:00")),
+                        new SubjectTerm("Trial License")))
+                .setFolderName(GmailFolders.ALL_MAIL)
+                .setDirection(EmailReadDirection.BACKWARD);
+        
+        DataWriter writer = new FileWriter(new File("scratch/data/output/trials-" 
+                + new SimpleDateFormat("yyyy-MM-dd-HHmmss").format(new Date()) 
+                + ".bin"));
+        
+        Job.run(reader, writer);
+        
+        System.out.println(writer.getRecordCount() + " emails saved.");
+    }
+
+    
+    @Test
+    public void exportDumpedEmailsToExcel() throws Throwable {
+        DataReader reader = new FileReader(new File("scratch/data/output/trials-2017-08-09-183137.bin"));
+        
+        reader = new TransformingReader(reader)
+                .add(new SelectFields("from", "to", "sentDate", "subject", "content"))
+//                .add(new BasicFieldTransformer("sentDate").stringToDateTime("EEE MMM dd HH:mm:ss z yyyy"))
+                .add(new BasicFieldTransformer("content")
+                        .flattenToString("\n")
+                        .left(32767));
+        
+        
+        ExcelDocument document = new ExcelDocument();
+        DataWriter writer = new ExcelWriter(document).setSheetName("License Trials");
+
+        Job.run(reader, writer);
+
+        String date = new SimpleDateFormat("yyyy-MM-dd-HHmmss").format(new Date());
+        document.save(new File("scratch/data/output/trials-" + date + ".xlsx"));
+    }
+    
+}
